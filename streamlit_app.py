fd9cd0f44a5aa049c1cdf001a2639efeaeae30ba
h098s21015-ship-it <h098s21015@chunchon.gwe.hs.kr>
2025-10-30 15:09:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -196,7 +196,7 @@ with col1:
 
 with col2:
     st.write("유튜브 동영상 예제")
-    st.video("https://www.youtube.com/watch?v=QCBsuP-Uzt0")
+    st.video("https://www.youtube.com/watch?v=gfxLwYI5uxQ")
 
 # --- 10. 마무리 ---
 st.markdown("---")
